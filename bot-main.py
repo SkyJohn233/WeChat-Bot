0959392704437d6ec970c777f021ff457e7d9f72
unknown <skyjohn@pku.edu.cn>
2018-03-21 16:29:50 +0800

    add some function

--- bot-main.py
+++ bot-main.py
@@ -5,16 +5,50 @@ import time
 import itchat
 from itchat.content import *
 from get_room_ids import *
-
+import requests
+import random
+from datetime import datetime
 # FromUserNames in the group chat
 
 @itchat.msg_register(TEXT, isGroupChat=True)
 def group_text_reply(msg):
-	if(msg.User.NickName==u"鸡友群"):
-		if itchat.search_friends(userName=msg["FromUserName"])["NickName"] in chatroom_nicknames:
-			msg.user.send("%s: %s" % (msg.ActualNickName, msg.text))
+	if msg["FromUserName"] in chatroom_ids :
+		print(1)
+		print(msg)
+		search_answer = itchat.search_friends(userName=msg["ActualUserName"])
+		print(search_answer)
+		friend_name = search_answer["RemarkName"]
+		if friend_name is None or friend_name is "" :
+			friend_name =search_answer["NickName"]
+		if friend_name in chatroom_nicknames:
+			pass
+			# msg.user.send("%s: %s" % (msg.ActualNickName, "sb"))
 		else :
 			msg.user.send(u"Q酱棒棒哒！")
+	if msg["ToUserName"] in chatroom_ids:
+		msg.user.send(u"Q酱棒棒哒")
+
+
+@itchat.msg_register([PICTURE], isGroupChat=True)
+def group_picture_reply(msg):
+	print(msg)
+	if msg["FromUserName"] in chatroom_ids :
+		print(1.5)
+		search_answer = itchat.search_friends(userName=msg["ActualUserName"])
+		print(search_answer)
+		friend_name = search_answer["RemarkName"]
+		if friend_name is None or friend_name is "" :
+			friend_name =search_answer["NickName"]
+		if friend_name in chatroom_nicknames:
+			# msg.user.send("%s%s" % (msg.ActualNickName, "sb"))
+			sticking_num = 9
+			sticking =random.randint(1,sticking_num)
+			print(sticking)
+			msg.user.send_image(str(sticking)+".jpg")
+		else:
+			msg.user.send(u"Q酱大帅比")
+	if msg["ToUserName"] in chatroom_ids:
+		msg.user.send(u"Q酱大帅比")
 
 
 caocao =  "@0c04671674deb44d6974644b4ff6f968f1c8d5d1b639fb8a48e0d77cb1b51cbf"
@@ -25,8 +59,17 @@ def text_reply(msg):
 	print(msg)
 	if msg['ToUserName'] == "filehelper":
 		itchat.send(u"test", toUserName=msg["ToUserName"])
+		url = msg.text
+		try:
+			r=requests.get(url,verify=False)
+			filepath = r"C:\Users\skyjohn\Desktop\wechat"
+			filepath += '\\'+url.split('/')[-1]
+			with open(r"C:\Users\skyjohn\Desktop\wechat\1.pdf","wb") as file_:
+				file_.write(r.content)
+		except:
+			print("Url download failed") 		
 	if msg["FromUserName"] == caocao:
-		if(msg.text==u"口一哈"):
+		if(msg.text==u"口一哈"):	
 			msg.user.send(u"mua")
 		else:
 			msg.user.send('%s: %s' % (msg.type, msg.text))
@@ -34,7 +77,36 @@ def text_reply(msg):
 		msg.user.send(u"叶志晟牛逼！")
 
 
+my_uuid = "@a0d8a365cce9931ab7c85880bd3e2272bce8138090d10fbd300a3948554ee5e1"
+@itchat.msg_register([PICTURE], isGroupChat=False)
+def picture_reply(msg):
+	print(2.5)
+	print(msg)
+	picture_sender=msg["FromUserName"]
+	if picture_sender in[ caocao, shenshen] :
+		sticking_num = 9
+		sticking =random.randint(1,sticking_num)
+		print(sticking)
+		itchat.send_image(str(sticking)+".jpg", toUserName=picture_sender)
+	if picture_sender == my_uuid:
+		sticking_num = 9
+		sticking = random.randint(1,sticking_num)
+		print("2.5.2")
+		itchat.send("send image"+str(sticking)+".jpg",toUserName="filehelper")
+		itchat.send_image(str(sticking)+".jpg",toUserName="filehelper")
+
+
+
+@itchat.msg_register([ATTACHMENT])
+def download_files(msg):
+	if(msg["ToUserName"]!="filehelper"): return None
+	print(msg)
+	msg.download(msg.filename)
+
 itchat.auto_login(True)
-itchat.send(u"test",toUserName="caoshengcao1006")
 itchat.send(u"auto reply begin", toUserName="filehelper")
+random.seed(time.time())
+# print(itchat.search_friends(userName="@dea0e53b8d4678cbb5c685953ebd2973ed99e9511c70999e7bbafbf4d35ea467"))
+# print(itchat.search_friends(userName=shenshen))
+# print(itchat.search_friends(userName=caocao))
 itchat.run()
--- get_room_ids.py
+++ get_room_ids.py
@@ -3,3 +3,5 @@
 
 chatroom_nicknames = [u"吴翰宇",u"老大",u"李狗蛋",u"大屁颠",u"五月",u"张钰清",u"马启程",u"潘凯航",
 					  u"沈泽文",u"崔祎楠",u"杨文强"]
+
+chatroom_ids = ["@@184bbc6f24b7039f82eb31a32e6ac1ab640f50ed232f418e2a3237946f35b971"]
